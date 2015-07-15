072596101ebdb1265a79ad58dcf5e71bbd389bd0
Tanya Breshears <tanya.breshears@airbnb.com>
2015-07-14 20:12:56 -0700

    First pass at flame effects

--- node/flames_test.js
+++ node/flames_test.js
@@ -1,17 +1,49 @@
+var OPC = new require('./opc');
+var opc = new OPC(process.argv[2] || 'localhost', 7890);
+
 var Canvas = require('canvas'),
     Image = Canvas.Image,
     canvas = new Canvas(800, 400),
-    ctx = canvas.getContext('2d'),
+    context = canvas.getContext('2d'),
     fs = require('fs');
+
+
+var interval = (canvas.width - 100) / 60,
+    startingPoint = 50,
+    y = canvas.height / 2;
 
 console.log("Trying to read file");
 
+var img = new Image;
+
 fs.readFile('../images/flames.jpg', function(err, flames){
   if (err) throw err;
-  img = new Image;
   img.src = flames;
-  ctx.drawImage(img, 0, 0, img.width, img.height);
-  console.log("Wrote image to canvas!");
-  console.log("Buffer size: " + canvas.toBuffer().length);
-
+  console.log("Successfully read image!");
 });
+
+function draw() {
+  var imHeight = img.height * canvas.width / img.width;
+
+  // Scroll down slowly, and wrap around
+  var speed = 0.05;
+  var y = ((new Date()).getTime() * -speed) % imHeight;
+
+  //context.drawImage(img, 0, 0, img.width, img.height);
+
+  // Use two copies of the image, so it seems to repeat infinitely
+  context.drawImage(img, 0, y, canvas.width, imHeight);
+  context.drawImage(img, 0, y + imHeight, canvas.width, imHeight);
+
+  for (var i = 0; i < 60; i++) {
+    var x =  startingPoint + (i * interval);
+    console.log("Getting pixel color at: " + x + ", " + y);
+    var rgb = context.getImageData(x, y, 1, 1);
+    console.log("Setting pixel to: " + rgb.toString());
+    opc.setPixel(i, rgb[0], rgb[1], rgb[2]);
+  }
+
+  opc.writePixels();
+}
+
+setInterval(draw, 50);
