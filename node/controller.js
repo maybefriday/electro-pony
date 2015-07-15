3665879b593eebb7e17e1392eb384dd3076be7e0
Tanya Breshears <tanya.breshears@airbnb.com>
2015-07-14 21:05:08 -0700

    Remapping buttons and more logging

--- node/controller.js
+++ node/controller.js
@@ -5,8 +5,8 @@ var OPC = new require('./opc'),
     opc = new OPC('localhost', 7890),
     GPIO = require('onoff').Gpio,
     button1 = new GPIO(24, 'in', 'both'),
-    button2 = new GPIO(22, 'in', 'both'),
-    button3 = new GPIO(23, 'in', 'both'),
+    button2 = new GPIO(23, 'in', 'both'),
+    button3 = new GPIO(22, 'in', 'both'),
     button4 = new GPIO(18, 'in', 'both'),
     intervalId = -1;
 
@@ -40,6 +40,7 @@ function sing(err, state) {
 
 function darkness(err, state) {
   if (state == 1) {
+    console.log("Turn off all the things.");
     cancelCurrentEffect();
 
     for (var i = 0; i < (numStrips * ledsPerStrip); i++) {
