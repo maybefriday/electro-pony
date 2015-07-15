2eff2565ece9670f9aee07caa89d76761d907d48
Tanya Breshears <tanya.breshears@airbnb.com>
2015-07-14 19:25:29 -0700

    Breaking tests out more logically

--- node/button-test.js
+++ node/button-test.js
@@ -3,6 +3,22 @@ var GPIO = require('onoff').Gpio,
     led = new GPIO(25, 'out'),
     button = new GPIO(24, 'in', 'both');
 
+var GpioStream = require('gpio-stream');
+
+// Buttons: 18, 22, 23, 24
+// LED: 25
+
+var button1 = GpioStream.readable(18);
+    //button2 = GpioStream.readable(22),
+    //button3 = GpioStream.readable(23),
+    //button4 = GpioStream.readable(24);
+
+// pipe the button presses to stdout
+button1.pipe(process.stdout);
+//button2.pipe(process.stdout);
+//button3.pipe(process.stdout);
+//button4.pipe(process.stdout);
+
 // define the callback function
 function light(err, state) {
 
--- node/rainbow_fade.js
+++ node/rainbow_fade.js
@@ -6,45 +6,19 @@
 var OPC = new require('./opc');
 var model = OPC.loadModel('../layouts/strip64.json');
 var opc = new OPC(process.argv[2] || 'localhost', 7890);
-var GpioStream = require('gpio-stream');
 
-var Canvas = require('canvas'),
-    Image = Canvas.Image,
-    canvas = new Canvas(800, 400),
-    ctx = canvas.getContext('2d');
+var GPIO = require('onoff').Gpio,
+    button = new GPIO(24, 'in', 'both'),
+    led = new GPIO(25, 'out');
 
-// Buttons: 18, 22, 23, 24
-// LED: 25
-
-var button1 = GpioStream.readable(18);
-    //button2 = GpioStream.readable(22),
-    //button3 = GpioStream.readable(23),
-    //button4 = GpioStream.readable(24);
-
-// pipe the button presses to stdout
-button1.pipe(process.stdout);
-//button2.pipe(process.stdout);
-//button3.pipe(process.stdout);
-//button4.pipe(process.stdout);
-
-console.log("Trying to read file");
-
-fs.readFile(__dirname + '../images/flames.jpg', function(err, flames){
-  if (err) throw err;
-  img = new Image;
-  img.src = flames;
-  ctx.drawImage(img, 0, 0, img.width, img.height);
-  console.log("Wrote image to canvas!");
-  console.log("Buffer size: " + canvas.toBuffer().length);
-
-});
+var brightness = 0.3;
 
 function draw() {
 
     for (var i = 0; i < 192; i++) {
       var d = new Date();
       var hue = ((d.getTime() * 0.01 + i * 2.0) % 100) / 100;
-      rgb = OPC.hsv(hue, 0.2, 0.8);
+      rgb = OPC.hsv(hue, 0.2, brightness);
       opc.setPixel(i, Math.round(rgb[0]), Math.round(rgb[1]), Math.round(rgb[2]));
     }
 
@@ -52,18 +26,26 @@ function draw() {
 
 }
 
-function updateColor(err, state) {
+function updateBrightness(err, state) {
 
   // check the state of the button
   // 1 == pressed, 0 == not pressed
   if(state == 1) {
     // turn LED on
     led.writeSync(1);
+
+    // Make LED strips brighter while button is pressed
+    brightness = 0.9;
+
   } else {
     // turn LED off
     led.writeSync(0);
+
+    // Return the LEDs to dimmer level
+    brightness = 0.3;
   }
 
 }
 
-setInterval(draw, 100);
+button.watch(updateBrightness);
+setInterval(draw, 50);
