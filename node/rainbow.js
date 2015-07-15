6c12fd780e0768019ffd2bb3ab16e386be0cb4d2
Tanya Breshears <tanya.breshears@airbnb.com>
2015-07-14 21:54:32 -0700

    Debugging rainbow

--- node/rainbow.js
+++ node/rainbow.js
@@ -12,14 +12,15 @@ var Rainbow = function(opc, numLeds)
 };
 
 Rainbow.prototype.go = function() {
-  return setInterval(this._draw, 50);
+  return setInterval(this._draw, 33);
 }
 
 Rainbow.prototype._draw = function() {
 
     for (var i = 0; i < _this.numLeds; i++) {
       var d = new Date();
-      var hue = ((d.getTime() * 0.01 + i * 2.0) % 100) / 100;
+      var hue = ((d.getTime() * 0.01 + i * 2.0) % 100) / 100.0;
+      console.log("setting hue to : " + hue);
       rgb = OPC.hsv(hue, 0.2, 0.8);
       _this.opc.setPixel(i, rgb[0], rgb[1], rgb[2]);
     }
