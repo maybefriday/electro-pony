66f15fa46ba498610a55f48d2cd20c01b8cb1e73
Tanya Breshears <tanya.breshears@airbnb.com>
2015-07-14 20:27:34 -0700

    Supporting 3 strips

--- node/flames_test.js
+++ node/flames_test.js
@@ -10,7 +10,9 @@ var Canvas = require('canvas'),
 
 var interval = (canvas.width - 100) / 60,
     startingPoint = 50,
-    centerY = canvas.height / 2;
+    strip1Y = canvas.height / 4,
+    strip2Y = canvas.height * 2 / 4,
+    strip3Y = canvas.height * 3 / 4;
 
 console.log("Trying to read file");
 
@@ -37,10 +39,15 @@ function draw() {
 
   for (var i = 0; i < 60; i++) {
     var x =  startingPoint + (i * interval);
-    console.log("Getting pixel color at: " + x + ", " + centerY);
-    var rgb = context.getImageData(x, centerY, 1, 1).data;
-    console.log("Setting pixel to: " + rgb.toString());
-    opc.setPixel(i, rgb[0], rgb[1], rgb[2]);
+    //console.log("Getting pixel color at: " + x + ", " + centerY);
+    var rgb1 = context.getImageData(x, strip1Y, 1, 1).data;
+    var rgb2 = context.getImageData(x, strip2Y, 1, 1).data;
+    var rgb3 = context.getImageData(x, strip3Y, 1, 1).data;
+    //console.log("Setting pixel to: " + rgb[0] + ", " + rgb[1] + ", " + rgb[2]);
+    opc.setPixel(i, rgb1[0], rgb1[1], rgb1[2]);
+    opc.setPixel(i + 60, rgb2[0], rgb2[1], rgb2[2]);
+    opc.setPixel(i + 120, rgb3[0], rgb3[1], rgb3[2]);
+
   }
 
   opc.writePixels();
